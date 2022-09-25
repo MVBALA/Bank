5c76dfe6bf09d037135d661f8075ef03d9013dfa
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-09-25 22:58:44 +0530

    it is udemy autoboxing and unboxing exercise.

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,71 @@
+import java.util.ArrayList;
+
 public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        } else
+            return false;
+
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double intialAmount) {
+        Branch existingBranch = findBranch(branchName);
+        if (existingBranch != null) {
+            existingBranch.newCustomer(customerName, intialAmount);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount) {
+        Branch existingBranch = findBranch(branchName);
+        if (existingBranch != null) {
+            existingBranch.addCustomerTransaction(customerName, amount);
+            return true;
+        }
+        return false;
+    }
+
+    private Branch findBranch(String branchName) {
+        for (int i = 0; i < branches.size(); i++) {
+            Branch checkBranches = this.branches.get(i);
+            if (checkBranches.getName().equals(branchName)) {
+                return checkBranches;
+            }
+        }
+        return null;
+
+    }
+
+    public boolean listCustomers(String branchName, boolean print) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            System.out.println("Customer details for branch " + branchName);
+            ArrayList<Customer> branchCustomer = branch.getCustomers();
+            for (int i = 0; i < branchCustomer.size(); i++) {
+                Customer customer = branchCustomer.get(i);
+                System.out.println("Customer:" + customer.getName() + "[" + (i + 1) + "]");
+                if (print) {
+                    System.out.println("Transactions");
+                    ArrayList<Double> transactions = customer.getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j + 1) + "] Amount " + transactions.get(j));
+                    }
+                }
+            }
+            return true;
+        } else
+            return false;
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bank bank = new Bank("National Australia Bank");
+
+        bank.addBranch("Adelaide");
+
+        bank.addCustomer("Adelaide", "Tim", 50.05);
+        bank.addCustomer("Adelaide", "Mike", 175.34);
+        bank.addCustomer("Adelaide", "Percy", 220.12);
+
+        bank.addCustomerTransaction("Adelaide", "Tim", 44.22);
+        bank.addCustomerTransaction("Adelaide", "Tim", 12.44);
+        bank.addCustomerTransaction("Adelaide", "Mike", 1.65);
+
+        Bank bank1 = new Bank("National Bank");
+
+        bank1.addBranch("salakadai");
+
+        bank1.addCustomer("salakadai", "naveen", 500.00);
+        bank1.addCustomer("salakadai", "tamilarasi", 38.45);
+        bank1.addCustomer("salakadai", "siva", 456.76);
+
+        bank1.addCustomerTransaction("salakadai", "naveen", 1000.75);
+        bank1.addCustomerTransaction("salakadai", "tamilarasi", 10040.75);
+        bank1.addCustomerTransaction("salakad", "si", 1000.75);
+
+        bank.listCustomers("Adelaide", true);
+        System.out.println("-----------------");
+        bank1.listCustomers("salakadai", false);
+        System.out.println("-----------------");
+        bank1.listCustomers("salakad", true);
     }
 }
